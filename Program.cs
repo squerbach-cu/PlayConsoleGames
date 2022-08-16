03b90dda92c353a1165f9b846a83b69a69814105
squerbach-cu <s.querbach@computeruniverse.net>
2022-08-16 11:18:16 +0200

    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TowersOfHanoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
